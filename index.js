148540035b18fda7f727e369b06b64bfda9ed5fd
Pravin Pagare <pravinpgr9@gmail.com>
2025-04-19 15:52:42 +0530

    Updated the codebase

--- index.js
+++ index.js
@@ -38,16 +38,16 @@ app.use(helmet());
 // Use morgan for logging requests
 app.use(morgan("common"));
 
-// app.get("/",(req,res)=>{
-//     res.send("welcome to rest API")
-// })
+app.get("/",(req,res)=>{
+    res.send("welcome to rest API")
+})
 
-// app.use("/api/user",userRoute)
-// app.use("/api/auth",authRoute)
-// app.use("/api/posts",postRoute)
-// app.use("/api/emailverify",verifyRoute)
-// app.use("/api/posts",postRoute)
-// app.use("/api/linkedin",linkedRoute)
+app.use("/api/user",userRoute)
+app.use("/api/auth",authRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/emailverify",verifyRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/linkedin",linkedRoute)
 
 app.use("/api/tourismenquiry",tourism_enquiryRoute)
 
